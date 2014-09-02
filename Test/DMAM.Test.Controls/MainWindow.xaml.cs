719256655d14096e44b83cf64bd860b594d1b3f6
Andrew Block <andrew@blockcity.net>
2014-09-01 21:27:28 -0700

    Updated the DMAM.Test.Controls project to test harness the FieldValueEditor control.

--- Test/DMAM.Test.Controls/MainWindow.xaml.cs
+++ Test/DMAM.Test.Controls/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ namespace DMAM.Test.Controls
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModel();
         }
     }
 }
---
+++ Test/DMAM.Test.Controls/ViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+using DMAM.Core.MVVM;
+using DMAM.DataModels;
+
+namespace DMAM.Test.Controls
+{
+    public class ViewModel : ViewModelBase
+    {
+        private readonly FieldValue _testFieldValue1 = new FieldValue("ArtistName", "Artist Name", true);
+        private readonly FieldValue _testFieldValue2 = new FieldValue("AlbumTitle", "Album Title", true);
+        private readonly FieldValue _testFieldValue3 = new FieldValue("Year", "Year", true);
+        private readonly FieldValue _testFieldValue4 = new FieldValue("Label", "Label", true);
+        private readonly FieldValue _testFieldValue5 = new FieldValue("Copyright", "Copyright", true);
+
+        public ViewModel()
+        {
+            _testFieldValue1.SetOriginalValue("Dave Matthews Band");
+            _testFieldValue2.SetOriginalValue("Before These Crowded Streets");
+            _testFieldValue3.SetOriginalValue("1998");
+            _testFieldValue4.SetOriginalValue("RCA Records");
+            _testFieldValue5.SetOriginalValue("Copyright © 1998");
+        }
+
+        public FieldValue TestFieldValue1
+        {
+            get
+            {
+                return _testFieldValue1;
+            }
+        }
+
+        public FieldValue TestFieldValue2
+        {
+            get
+            {
+                return _testFieldValue2;
+            }
+        }
+
+        public FieldValue TestFieldValue3
+        {
+            get
+            {
+                return _testFieldValue3;
+            }
+        }
+
+        public FieldValue TestFieldValue4
+        {
+            get
+            {
+                return _testFieldValue4;
+            }
+        }
+
+        public FieldValue TestFieldValue5
+        {
+            get
+            {
+                return _testFieldValue5;
+            }
+        }
+    }
+}
