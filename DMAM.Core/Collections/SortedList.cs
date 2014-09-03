e5b7abfb98153cdc86f214d2b6cb12468212c25c
Andrew Block <andrew@blockcity.net>
2014-09-02 22:18:07 -0700

    Created the SortedList static class in DMAM.Core.
    SortedList extends IList<T> in order to provide sorted-insert operations.

---
+++ DMAM.Core/Collections/SortedList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace DMAM.Core.Collections
+{
+    public static class SortedList
+    {
+        public static void InsertSorted<T>(this IList<T> list, T item, IComparer<T> comparer)
+        {
+            var sortedItems = new List<T>(list);
+            sortedItems.Add(item);
+            sortedItems.Sort(comparer);
+
+            var index = sortedItems.IndexOf(item);
+            list.Insert(index, item);
+        }
+
+        public static void InitializeSorted<T>(this IList<T> list, IEnumerable<T> items, IComparer<T> comparer)
+        {
+            if (list.Count != 0)
+            {
+                list.Clear();
+            }
+
+            var sortedItems = new List<T>(items);
+            sortedItems.Sort(comparer);
+
+            foreach (var sortedItem in sortedItems)
+            {
+                list.Add(sortedItem);
+            }
+        }
+    }
+}
